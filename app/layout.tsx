4e9e4a7d45d0b4c9887ca210aaf5d5eeeb3b6b7e
erikahlback <eriahl25@kxfh.se>
2026-05-15 17:46:13 +0200

    Hej!

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,7 +46,25 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} ${instrumentSerif.variable} ${pressStart.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col bg-background text-foreground">
-        {children}
+        {/* Dekorativa figurer vänster */}
+        <img
+          src="/deco-left.png"
+          alt=""
+          aria-hidden="true"
+          className="fixed left-0 top-0 h-full w-auto object-cover object-top pointer-events-none select-none opacity-60"
+          style={{ zIndex: 0 }}
+        />
+        {/* Dekorativa figurer höger */}
+        <img
+          src="/deco-right.png"
+          alt=""
+          aria-hidden="true"
+          className="fixed right-0 top-0 h-full w-auto object-cover object-top pointer-events-none select-none opacity-60"
+          style={{ zIndex: 0 }}
+        />
+        <div className="relative" style={{ zIndex: 1 }}>
+          {children}
+        </div>
       </body>
     </html>
   );
