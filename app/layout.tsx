979cef5b2b38c08cc9811b90b1ef8cbf9f9c607d
erikahlback <eriahl25@kxfh.se>
2026-05-18 13:30:57 +0200

    New design

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,21 +46,21 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} ${instrumentSerif.variable} ${pressStart.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col bg-background text-foreground">
-        {/* Eld vänster – döljs på mobil */}
+        {/* Dekorativa figurer vänster – döljs på mobil */}
         <img
-          src="/fire-left.png"
+          src="/deco-left.png"
           alt=""
           aria-hidden="true"
           className="hidden lg:block fixed left-0 top-0 pointer-events-none select-none"
-          style={{ zIndex: 0, height: "100vh", width: "auto", objectFit: "cover", objectPosition: "left top" }}
+          style={{ zIndex: 0, height: "100vh", width: "auto" }}
         />
-        {/* Eld höger – döljs på mobil */}
+        {/* Dekorativa figurer höger – döljs på mobil */}
         <img
-          src="/fire-right.png"
+          src="/deco-right.png"
           alt=""
           aria-hidden="true"
           className="hidden lg:block fixed right-0 top-0 pointer-events-none select-none"
-          style={{ zIndex: 0, height: "100vh", width: "auto", objectFit: "cover", objectPosition: "right top" }}
+          style={{ zIndex: 0, height: "100vh", width: "auto" }}
         />
         <div className="relative" style={{ zIndex: 1 }}>
           {children}
